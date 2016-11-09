d2f68c039402c2c4afb0bfe27d77d90dda55be52
Ivan <vana.shimko@gmail.com>
2016-11-09 10:23:39 +0300

    Create solution structure

---
+++ Mapper/DtoMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper
+{
+    public class DtoMapper : IMapper
+    {
+        public TDestination Map<TSource, TDestination>(TSource source) where TDestination : new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
