33cf8c43100f6b4be8076f514db1fa9f01a1a74f
Ivan <vana.shimko@gmail.com>
2016-11-09 22:02:43 +0300

    Add project for testing with xUnit

---
+++ MapperTests/DtoMapperTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper.Tests
+{
+    public class DtoMapperTests
+    {
+        [Fact()]
+        public void MapTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
