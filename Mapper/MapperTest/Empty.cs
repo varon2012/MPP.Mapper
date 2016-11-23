623d3425258a76ef0c0127c13306fc6d8278a6a3
Оксана <oksana.piskunova@gmail.com>
2016-11-24 01:36:27 +0300

    Added tests for Mapper

---
+++ Mapper/MapperTest/Empty.cs
@@ -0,0 +1,15 @@
+﻿namespace MapperTest
+{
+    internal class Empty
+    {
+        public override bool Equals(object obj)
+        {
+            return obj is Empty;
+        }
+
+        public override int GetHashCode()
+        {
+            return 6;
+        }
+    }
+}
