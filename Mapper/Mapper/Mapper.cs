6fda543835c30ba2886e30cfbc67c1e01696e005
Oleg Pyatko <opyatko@apro-it.eu>
2016-12-07 00:27:43 +0300

    add getting properties from objects using reflection

--- Mapper/Mapper/Mapper.cs
+++ Mapper/Mapper/Mapper.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Mapper.Contracts;
 
 namespace Mapper
 {
-    public class Class1
+    public class Mapper : IMapper
     {
+        public TDestination Map<TSource, TDestination>(TSource source) where TDestination : new()
+        {
+            var typeSource = typeof(TSource);
+            return default(TDestination);
+        }
     }
 }
---
+++ Mapper/Mapper/ReflectionParser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Mapper.Contracts;
+
+namespace Mapper
+{
+    internal class ReflectionParser : IReflectionParser
+    {
+        public List<PropertyInfo> GetSameProperties<TSource, TDestination>(TSource sourceType, TDestination destinationType)
+        {
+            var list = new List<PropertyInfo>();
+            var sourceProperties = GetProperties(typeof(TSource));
+            var destinationSource = GetProperties(typeof(TDestination));
+            foreach (var sourceProperty in sourceProperties)
+            {
+                foreach (var destinationProperty in destinationSource)
+                {
+                    if (IsCanMap(sourceProperty, destinationProperty))
+                    {
+                        list.Add(destinationProperty);
+                        break;
+                    }
+                }
+            }
+            return list;
+        }
+
+        private PropertyInfo[] GetProperties(Type type)
+        {
+            return type.GetProperties(BindingFlags.Public
+                                                | BindingFlags.SetProperty
+                                                | BindingFlags.Instance);
+        }
+
+        private bool IsCanMap(PropertyInfo sourceProperty, PropertyInfo destinationProperty)
+        {
+            if (!sourceProperty.Name.Equals(destinationProperty.Name))
+            {
+                return false;
+            }
+            if (destinationProperty.GetSetMethod() == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
